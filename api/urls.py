5b41e476d77893c870944ac858a953617ef1ec22
Павел Шевель <pawel.pavlenti@yandex.ru>
2023-03-12 23:12:35 +0300

    фиксю баги

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (BooksViewSet, ReaderListView, RentLateReturnView,
+from .views import (BooksViewSet, ReaderListViewSet, RentLateReturnViewSet,
                     UserViewSet)
 
 app_name = 'api'
@@ -9,8 +9,8 @@ app_name = 'api'
 router = DefaultRouter()
 router.register('books', BooksViewSet, basename='books')
 router.register('users', UserViewSet, basename='users')
-router.register('readers', ReaderListView, basename='readers')
-router.register('rents', RentLateReturnView, basename='rents')
+router.register('readers', ReaderListViewSet, basename='readers')
+router.register('rents', RentLateReturnViewSet, basename='rents')
 
 urlpatterns = [
     path('', include(router.urls)),
