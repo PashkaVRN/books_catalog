8dfe8f4c825dbcf96968017d0263270f2ae85552
Pavel Shevel <pavelshevel96@gmail.com>
2023-04-25 20:57:59 +0300

    Начала систему бронирования книг пользователями

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (BooksViewSet, RentLateReturnViewSet, UserViewSet)
+from .views import (BookReservedViewSet, BooksViewSet, RentLateReturnViewSet,
+                    UserViewSet)
 
 app_name = 'api'
 
@@ -9,6 +10,7 @@ router_v1 = DefaultRouter()
 router_v1.register('books', BooksViewSet, basename='books')
 router_v1.register('users', UserViewSet, basename='users')
 router_v1.register('rents', RentLateReturnViewSet, basename='rents')
+router_v1.register('reserved', BookReservedViewSet, basename='reserved')
 
 urlpatterns = [
     path('', include(router_v1.urls)),
